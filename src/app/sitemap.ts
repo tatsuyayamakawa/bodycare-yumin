359f556029b5ec717349504dbd1743cfcb34ea31
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2023-07-18 08:23:40 +0900

    fix: sitemap.xmlのURLがローカルになっていたため独自ドメインに修正

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,12 @@
 import { MetadataRoute } from 'next'
+import Data from "@/data/data.json";
+
+const data = Data.data;
 
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'http://localhost:3000',
+			url: `${data.info.domain}`,
 			lastModified: new Date(),
 		},
 	]
