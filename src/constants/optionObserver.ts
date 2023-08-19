c1d7311005630feca8b33cc55b027ec0c15b2f4e
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2023-08-19 14:30:10 +0900

    update: アニメーションの発火範囲少し拡大

--- src/constants/optionObserver.ts
+++ src/constants/optionObserver.ts
@@ -1,7 +1,7 @@
 export const observerOptions = {
   mediaQueryWidth: 768,
-  largeScreenRootMargin: '-10% 0px',
-  smallScreenRootMargin: '-10% 0px',
+  largeScreenRootMargin: '10% 0px',
+  smallScreenRootMargin: '20% 0px',
   threshold: 0.5,
   once: true,
 };
