58898ce5c0fc6dac89c649b590ae5d3a1aa0e967
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2023-08-20 11:55:38 +0900

    feat: メニューに施術の流れ動画の追加

--- src/components/lp/Menu.tsx
+++ src/components/lp/Menu.tsx
@@ -1,12 +1,25 @@
+'use client';
+
+import { useState } from 'react';
 import { ScrollAnimator } from 'react-animate-observer';
+
+import { Button, Dialog, DialogBody, DialogFooter, Typography } from '@material-tailwind/react';
+import { FaYoutube } from 'react-icons/fa6';
 
 import { SectionHeading } from 'src/components/elements/heading/SectionHeading';
 import { ImageWrapper } from 'src/components/elements/image/ImageWrapper';
 import { SvgWaveTop, SvgWaveBottom } from 'src/components/elements/image/SvgWave';
+import { ExternalLink } from 'src/components/elements/link/ExternalLink';
 import { menus } from 'src/constants/menus';
 import { observerOptions } from 'src/constants/optionObserver';
 
 export const Menu = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <section id="menu">
       <div className="botom-0 left-0 w-full overflow-hidden">
@@ -45,8 +58,37 @@ export const Menu = () => {
                   </h4>
                   <h5 className="mt-1 text-center font-zenmincho text-base font-bold text-primary">{menu.price}</h5>
                   <p className="mt-4 text-justify font-notojp text-sm font-normal not-italic leading-normal tracking-wide text-primary">{menu.content}</p>
-                  <div className="mt-4 flex w-full">
-                    <div className="rounded-lg bg-secondary/80 px-3 py-2 font-notojp text-sm not-italic tracking-wide text-primary">{menu.option}</div>
+                  <div className="mt-4 flex w-full justify-between">
+                    <div className="flex items-center rounded-lg bg-secondary/80 px-3 py-3 font-notojp text-sm not-italic tracking-wide text-primary">{menu.option}</div>
+                    {index === 1 && (
+                      <Button size="sm" variant="outlined" className="border border-gray-25 shadow-none" onClick={handleClick}>
+                        <div className="flex items-center gap-2">
+                          <FaYoutube size={24} color="red" />
+                          <Typography variant="small" className="font-notojp font-normal not-italic tracking-wide text-gray-50">
+                            施術の流れ
+                          </Typography>
+                        </div>
+                      </Button>
+                    )}
+                    <Dialog size="lg" open={open} handler={handleClick}>
+                      <DialogBody className="p-0">
+                        <video className="h-full w-full rounded-t-lg" controls autoPlay muted>
+                          <source src="video/basic.mp4" type="video/mp4" />
+                        </video>
+                      </DialogBody>
+                      <DialogFooter className="justify-end">
+                        <ExternalLink url="https://youtu.be/xZo39vNkaUk" ariaLabel="YOUTUBEで見る" icon={false}>
+                          <Button size="sm" variant="outlined" className="border border-gray-25 shadow-none">
+                            <div className="flex items-center gap-2">
+                              <FaYoutube size={24} color="red" />
+                              <Typography variant="small" className="font-notojp font-normal not-italic tracking-wide text-gray-50">
+                                YOUTUBEで見る
+                              </Typography>
+                            </div>
+                          </Button>
+                        </ExternalLink>
+                      </DialogFooter>
+                    </Dialog>
                   </div>
                 </div>
               </ScrollAnimator>
