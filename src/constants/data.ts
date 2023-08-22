11c4b1e5d696f2967ebdcd9ec099d391122fc054
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2023-08-22 17:44:23 +0900

    update: descriptionの変更

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -4,7 +4,7 @@ export const data = {
     domain: 'https://bodycare-yumin.com',
     image: '/ogp.png',
     title: '手もみ整体 癒眠｜山形市で自律神経を整えるための整体サロン',
-    description: '手もみ整体 癒眠では、日々の生活やストレスで乱れがちな自律神経の改善と、健康な体を取り戻すための自然治癒力を高めるサポートをいたします。',
+    description: '手もみ整体 癒眠では、日々のストレスや不規則な生活習慣で乱れがちな自律神経の調整と健康な体を取り戻すためのお手伝いをいたします。',
   },
   sns: {
     instagram: 'https://www.instagram.com/bodycare_yumin',
