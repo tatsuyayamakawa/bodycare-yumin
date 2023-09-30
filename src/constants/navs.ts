09b9cad1ef3fff828143294eaae39cd077041f5b
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2023-09-30 22:08:15 +0900

    Change of navigation menu contents

--- src/constants/navs.ts
+++ src/constants/navs.ts
@@ -7,17 +7,7 @@ type NavItem = {
 export const navs: NavItem[] = [
   {
     id: 1,
-    title: '癒眠について',
-    link: 'about',
-  },
-  {
-    id: 2,
-    title: '料金',
-    link: 'menu',
-  },
-  {
-    id: 3,
-    title: 'アクセス',
-    link: 'access',
+    title: 'お問い合わせ',
+    link: '/contact',
   },
 ];
