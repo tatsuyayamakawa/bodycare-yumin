2eb38a4e642509acf503b6cb248adafbee8ac3f9
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2023-09-30 21:44:02 +0900

    Remove progress bar

--- src/ui/progress-bar.tsx
+++
@@ -1,7 +0,0 @@
-import { useReadingProgress } from 'src/hooks/useReadingProgress';
-
-export const ProgressBar = () => {
-  const completion = useReadingProgress();
-
-  return <span style={{ width: `${completion}%` }} className="absolute h-1 w-full bg-accent" />;
-};
