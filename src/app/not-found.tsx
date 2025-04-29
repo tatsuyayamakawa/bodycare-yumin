d8135e0e90e426a6ac45ac03ca3b63d91ef91cf1
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2025-04-29 18:56:45 +0900

    feat: add error and not found pages
    - Created Error component to display a 500 server error message with a link to return to the homepage.
    - Created NotFound component to display a 404 not found message with a link to return to the homepage.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <div className="absolute inset-0 flex flex-col items-center justify-center space-y-8">
+      <h2 className="font-bebas text-muted-foreground/50 text-[clamp(3rem,10vw,8rem)] leading-none tracking-wider">
+        404 Not Found
+      </h2>
+      <p className="text-muted-foreground/50 text-[clamp(1rem,2.5vw,1.5rem)]">
+        お探しのページは見つかりませんでした。
+      </p>
+      <Button variant="outline" size="lg" asChild>
+        <Link href="/">トップページへ戻る</Link>
+      </Button>
+    </div>
+  );
+}
